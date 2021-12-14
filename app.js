9a04b77d017a88f1586dfdd59aca23ebfa07a257
Bharat Goyal <bgoyal1471@gmail.com>
2021-12-14 13:05:38 +0530

    chore : input-output processing done

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
 
 function clickHandler() {
-  console.log("clicked!");
-  console.log("input", txtInput.value);
+  outputDiv.innerText = "nsnssjnsnsjssssns " + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler);
