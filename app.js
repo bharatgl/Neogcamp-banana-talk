a0ed54cc289593f95d30c844f70a8d5ff850588a
Bharat Goyal <bgoyal1471@gmail.com>
2021-12-14 15:20:01 +0530

    feat: integrated minion translation

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler() {
-  outputDiv.innerText = "nsnssjnsnsjssssns " + txtInput.value;
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "	https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input 
 }
 
-btnTranslate.addEventListener("click", clickHandler);
+function errorHandler(error){
+    console.log("error occured", error);
+}
+
+
+function clickHandler(){
+    var inputText = txtInput.value;   // input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;   // output
+        })
+        .catch(errorHandler)
+};
+
+
+btnTranslate.addEventListener("click", clickHandler)
